795ad493ebef4fa203d50f27bbef4e84b082038c
guoxianghan <1146267220@qq.com>
2017-02-16 00:34:37 +0800

    项目初次发布

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using HTTPBrowser;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebBrowser web = new WebBrowser();
+            //ResponsePack_WebBrowser r = web.Request("https://www.baidu.com", null) as ResponsePack_WebBrowser;
+            string json = File.ReadAllText(@"C:\Users\Sier\Desktop\m.ctrip.com.har");
+            //string json = File.ReadAllText(@"C:\Users\Sier\Desktop\aio.liantuo.com.har");
+            HttpHar h = JsonConvert.DeserializeObject<HttpHar>(json);
+            Cookie[] c = new  Cookie[0];
+            h.log.entries[0].request.url = "https://123.sogou.com/";
+            var r = web.Request(h.log.entries[0].request, c);
+             
+            web.RefreshConfig();
+            RequestConfig req = new RequestConfig();
+        }
+    }
+}
